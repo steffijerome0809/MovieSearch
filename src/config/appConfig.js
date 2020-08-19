65296d19ed5713c1289302083aa7b8a4917d04ab
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-19 03:07:51 -0400

    setting up some default messages

---
+++ src/config/appConfig.js
@@ -0,0 +1,12 @@
+//setting up some default messages
+export const homepageTitle = "Search your favourite movies!";
+export const noDesc = "No description available";
+export const noDate = "Year not available";
+export const noResultsFoundLabel = "No matching results found.";
+export const noResultsFoundDesc = "Please try again with different keyword(s).";
+export const initialStateLabel = "Movie search results will appear here.";
+export const initialStateDesc =
+  "Use search feature to get your favourite movies.";
+export const errorOccurredLabel = "An unexpected error occurred";
+export const errorOccurredDesc =
+  "Please check your internet connection and/or try refreshing your browser.";

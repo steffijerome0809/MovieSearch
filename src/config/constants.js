a9a7d608683a56d9c5186d57a16ec368548e257a
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-19 02:59:43 -0400

    setting up the apikey and api domain and exported

---
+++ src/config/constants.js
@@ -0,0 +1,6 @@
+export const APP_NAME = 'Movie Search App';
+const API_DOMAIN = 'https://api.themovidb.org/3/search/movie';
+const API_KEY = 'cb0e2c81df73c5817dba3b61cf41d540';
+
+export const API_PATH = `${API_DOMAIN}?api_key=${API_KEY}&query=`;
+export const POSTER_BASE_PATH = 'https://image.tmdb.org/t/p/w300/';

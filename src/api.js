1fc72e9fdb2cc96a43165384ce148bfeef634638
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-19 02:44:40 -0400

    when the url is fetched the response is stored in data obj and the state of status code is updated

--- src/api.js
+++ src/api.js
@@ -5,4 +5,21 @@ const getData = async (url) => {
     data: {},
     statusCode: 0,
   };
+
+  // Introducing the try block of code
+
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    result.data = await response.json();
+    result.statusCode = 200;
+  } catch (error) {
+    console.error('An error occured :', error);
+    result.statusCode = 500;
+    return result;
+  }
 };

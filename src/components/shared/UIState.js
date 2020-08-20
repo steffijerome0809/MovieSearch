6faea78d45a601204226b4c8e7aa704148bb4271
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-20 10:35:23 -0400

    added the ui state

---
+++ src/components/shared/UIState.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from '@blueprintjs/core';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 48px 24px 0 24px;
+`;
+
+/**
+ * Creating a styled component to add customcolor
+ * prop to Blueprint's Icon element.
+ */
+const StyledIcon = styled(Icon)`
+  color: ${(props) => (props.customcolor ? props.customcolor : '#000')};
+`;
+
+const UIState = ({
+  icon,
+  iconSize,
+  intent = 'none',
+  customcolor = '#000',
+  title,
+  desc,
+}) => {
+  return (
+    <Wrapper>
+      <StyledIcon
+        icon={icon}
+        iconSize={iconSize}
+        intent={intent}
+        customcolor={customcolor}
+      />
+      <br />
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </Wrapper>
+  );
+};
+
+UIState.propTypes = {
+  icon: PropTypes.string,
+  iconSize: PropTypes.string,
+  intent: PropTypes.string,
+  customcolor: PropTypes.string,
+  title: PropTypes.string,
+  desc: PropTypes.string,
+};
+
+export default UIState;

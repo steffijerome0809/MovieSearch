5d71027c5fa0b4946a6b73bffa2a285edceb63a2
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-20 01:19:31 -0400

    added utils file js

---
+++ src/components/utils/utils.js
@@ -0,0 +1,18 @@
+const utils = {
+  /**
+   * @param {string} languageCode The language code of movie
+   * received from API.
+   *
+   * @param {Array.<Object>} languageList List of languages against
+   * which language code of each movie is checked and full language
+   * name retrieved.
+   */
+  getLanguageName: (languageCode, languageList) => {
+    const languageArr = languageList.filter(
+      (language) => language.code === languageCode
+    );
+    return languageArr[0] ? languageArr[0].name : languageCode;
+  },
+};
+
+export default utils;

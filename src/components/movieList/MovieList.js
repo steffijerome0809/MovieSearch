41be2505566dc9e1d5067f3ad0bd10bce378f8b2
steffijerome0809 <steffi.jerome@yahoo.com>
2020-08-20 01:31:45 -0400

    added the movie list types

---
+++ src/components/movieList/MovieList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import MovieInstance from '../movieInstance/MovieInstance';
+
+const MovieList = ({ movieList }) => {
+  return (
+    <>
+      {/** Looping through array to generate movie list on page */}
+      {Array.isArray(movieList) && movieList.length > 0
+        ? movieList.map((movie, index) => {
+            return (
+              <MovieInstance
+                key={index}
+                title={movie.original_title}
+                overview={movie.overview}
+                language={movie.original_language}
+                releaseDate={movie.release_date}
+                posterPath={movie.poster_path}
+              />
+            );
+          })
+        : ''}
+    </>
+  );
+};
+
+MovieList.propTypes = {
+  movieList: PropTypes.array,
+};
+
+export default MovieList;
